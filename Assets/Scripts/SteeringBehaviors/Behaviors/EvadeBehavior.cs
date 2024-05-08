32009ed0104fa32693a1a73ab4a30212fc593c44
gere149 <gere.delatorre@gmail.com>
2024-05-08 17:51:30 +0200

    Activitat 12, 13, 14
    Se ha intentado, pero creo que bastante bien.    :)

--- Assets/Scripts/SteeringBehaviors/Behaviors/EvadeBehavior.cs
+++ Assets/Scripts/SteeringBehaviors/Behaviors/EvadeBehavior.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class EvadeBehavior : Steering
 {
+    [SerializeField]
+    private float maxPrediction = 2f;
+
+    public GameObject targetObj;
+
     public override SteeringData GetSteering(SteeringBehaviorController steeringController)
     {
-        throw new System.NotImplementedException();
+        Vector2 direction = transform.position - targetObj.transform.position;
+        float distance = direction.magnitude;
+        float speed = GetComponent<Rigidbody2D>().velocity.magnitude;
+
+        float prediction;
+        if (speed <= distance / maxPrediction)
+            prediction = maxPrediction;
+        else
+            prediction = distance / speed;
+
+        Vector2 predictedTarget = (Vector2)targetObj.transform.position
+                + targetObj.GetComponent<Rigidbody2D>().velocity * prediction;
+
+        // Calculate the evasion direction
+        Vector2 evasionDirection = (Vector2)transform.position - predictedTarget;
+
+        return new SteeringData()
+        {
+            linear = evasionDirection.normalized * steeringController.maxAcceleration,
+            angular = 0
+        };
     }
 }
